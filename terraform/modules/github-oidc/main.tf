fcfa126d581a23af8d8fda2c6636bb2e8b2cfb39
Ranim <ranim.turki.rt@gmail.com>
2026-09-20 16:01:45 +0200

    Update iam role

--- terraform/modules/github-oidc/main.tf
+++ terraform/modules/github-oidc/main.tf
@@ -43,7 +43,7 @@ resource "aws_iam_role" "github_terraform" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
         }
       }
     }]
@@ -177,7 +177,7 @@ resource "aws_iam_role" "github_deploy" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
         }
       }
     }]
